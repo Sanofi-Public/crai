aa2b251d57445fdbc2f5ec9e4403fab917133965
Vincentx15 <vincent.mallet96@gmail.com>
2023-03-30 10:56:44 +0200

    fixed carving : missing fields in the header, weird interaction with mrcfile

--- prepare_data/process_data.py
+++ prepare_data/process_data.py
@@ -12,8 +12,9 @@ import numpy as np
 import scipy.ndimage
 import scipy.interpolate
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(script_dir, '..'))
+if __name__ == '__main__':
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    sys.path.append(os.path.join(script_dir, '..'))
 
 from utils.mrc_utils import load_mrc, pymol_parse
 
@@ -24,14 +25,18 @@ def carve(mrc, pdb_name, out_name='carved.mrc', padding=4, filter_cutoff=-1, ove
         The main steps are :
             - Getting the coordinates to get a box around the PDB
             - Selecting the right voxels in this box
-            - Updating the origin and the size of the 'cell'
             - Optionally filter out the values further away from filter_cutoff
+            - Creating a new mrc with the origin and the size of the 'cell'
     :param mrc: Either name or mrc file
     :param pdb_name: path to the pdb
-    :param out_name: path to the output mrc
+    :param out_name: path to the output mrc. If the extension is not .mrc but .map, the origin is not read correctly by
+        Chimera
     :param padding: does not need to be an integer
     :param filter_cutoff: negative value will skip the filtering step. Otherwise it's a cutoff in Angstroms
     """
+    if os.path.exists(out_name) and not overwrite:
+        return
+
     # Get the bounds from the pdb
     coords = pymol_parse(pdbname=pdb_name)
     xyz_min = coords.min(axis=0)
@@ -39,36 +44,45 @@ def carve(mrc, pdb_name, out_name='carved.mrc', padding=4, filter_cutoff=-1, ove
 
     # Load the mrc data
     # the data and the 'x,y,z' annotation might not match.
-    # axis_mapping tells us which 'xyz' dimension match which axis of the data : {first_axis : X, Y or Z}
     mrc = load_mrc(mrc, mode='r+')
-    axis_mapping = {0: int(mrc.header.maps) - 1,
-                    1: int(mrc.header.mapr) - 1,
-                    2: int(mrc.header.mapc) - 1}
-    reverse_axis_mapping = {value: key for key, value in axis_mapping.items()}
     voxel_size = mrc.voxel_size[..., None].view(dtype=np.float32)
     voxel_size = np.array(voxel_size)
     origin = mrc.header.origin[..., None].view(dtype=np.float32)
     origin = np.array(origin)
-    # The shift array convention is as crappy as the s,r,c order.
-    shift_array = np.array((mrc.header.nzstart,
-                            mrc.header.nystart,
-                            mrc.header.nxstart))
-    shift_array_xyz = np.array([shift_array[reverse_axis_mapping[i]] for i in range(3)])
+
+    # axis_mapping tells us which axis of the data matches which 'xyz' dimension :
+    # {first_axis (s) : X, Y or Z, second axis (r): x/y/z, third axis (c) : x/y/z}
+    # reverse axis mapping tells us which x,y,z is where in the data array
+    axis_mapping = (int(mrc.header.maps) - 1,
+                    int(mrc.header.mapr) - 1,
+                    int(mrc.header.mapc) - 1)
+    reverse_axis_mapping = {value: key for key, value in enumerate(axis_mapping)}
     data_shape_xyz = np.array([mrc.data.shape[reverse_axis_mapping[i]] for i in range(3)])
+
+    # The shift array convention is as crappy as the s,r,c order.
+    # I don't get it for now
+    shift_array = np.array((mrc.header.nzstart,  # nzstart always correspond to 'c'
+                            mrc.header.nystart,  # nystart always correspond to 'r'
+                            mrc.header.nxstart))  # nxstart always correspond to 's'
+    shift_array_xyz = np.array([shift_array[reverse_axis_mapping[i]] for i in range(3)])
+    # shift_array_new = np.array((mrc.header.nxstart,  # nxstart always correspond to 's'
+    #                             mrc.header.nystart,  # nystart always correspond to 'r'
+    #                             mrc.header.nzstart))  # nzstart always correspond to 'c'
+    # shift_array_xyz_new = np.array([shift_array_new[reverse_axis_mapping[i]] for i in range(3)])
+    # mins_array_new = ((xyz_min - origin) / voxel_size - shift_array_xyz_new).astype(int, casting='unsafe')
+    # maxs_array_new = ((xyz_max - origin) / voxel_size - shift_array_xyz_new).astype(int, casting='unsafe')
 
     # Using the origin, find the corresponding cells in the mrc
     mins_array = ((xyz_min - origin) / voxel_size - shift_array_xyz).astype(int, casting='unsafe')
     maxs_array = ((xyz_max - origin) / voxel_size - shift_array_xyz).astype(int, casting='unsafe')
     mins_array = np.max((mins_array, np.zeros_like(mins_array)), axis=0)
     maxs_array = np.min((maxs_array, data_shape_xyz - 1), axis=0)
-    x_min, y_min, z_min = mins_array
-    x_max, y_max, z_max = maxs_array
     grouped_bounds = [(i, j) for i, j in zip(mins_array, maxs_array)]
-    shifted_origin = origin + (mins_array + shift_array_xyz - (padding,) * 3) * voxel_size
+    shifted_origin = xyz_min - ((padding,) * 3) * voxel_size
 
     # Extract those cells, one must be careful because x is not always the columns index
     data = mrc.data.copy()
-    for array_axis, data_axis in axis_mapping.items():
+    for array_axis, data_axis in enumerate(axis_mapping):
         data = np.take(data, axis=array_axis, indices=range(*grouped_bounds[data_axis]))
 
     data = np.pad(data, pad_width=padding)
@@ -87,20 +101,19 @@ def carve(mrc, pdb_name, out_name='carved.mrc', padding=4, filter_cutoff=-1, ove
         filter_array = np.reshape(filter_array, data.shape)
         data = filter_array * data
 
-    try:
-        # Update meta-data and save
-        with mrcfile.new(out_name, overwrite=overwrite) as mrc:
-            mrc.header.cella.x = (x_max - x_min + 2 * padding) * voxel_size[0]
-            mrc.header.cella.y = (y_max - y_min + 2 * padding) * voxel_size[1]
-            mrc.header.cella.z = (z_max - z_min + 2 * padding) * voxel_size[2]
-            mrc.header.origin.x = shifted_origin[0]
-            mrc.header.origin.y = shifted_origin[1]
-            mrc.header.origin.z = shifted_origin[2]
-            mrc.set_data(data)
-            mrc.update_header_from_data()
-            mrc.update_header_stats()
-    except ValueError:
-        pass
+    # Dump an uncompressed file. Chimerax does not handle compressed mrc files
+    with mrcfile.new(out_name, overwrite=overwrite) as new_mrc:
+        data = np.transpose(data, axes=axis_mapping)
+        new_mrc.set_data(data)
+        new_mrc.header.maps = 1
+        new_mrc.header.mapr = 2
+        new_mrc.header.mapc = 3
+        new_mrc.update_header_from_data()
+        new_mrc.update_header_stats()
+        new_mrc.voxel_size = tuple(voxel_size)
+        new_mrc.header.origin = tuple(shifted_origin)
+        # new_mrc.header.mx is done when calling update header from data
+        # new_mrc.header.cella.x is done when updating voxel size
 
 
 def resample(mrc, padding=0, out_name='resample.mrc', overwrite=False, new_voxel_size=1):
@@ -111,7 +124,9 @@ def resample(mrc, padding=0, out_name='resample.mrc', overwrite=False, new_voxel
     :param out_name: Name of the mrc to dump
     :return: None
     """
-    mrc = load_mrc(mrc, mode='r+')
+    if os.path.exists(out_name) and not overwrite:
+        return
+    mrc = load_mrc(mrc, mode='r')
     voxel_size = mrc.voxel_size[..., None].view(dtype=np.float32)
     voxel_size = np.array(voxel_size)
     axis_mapping = {0: int(mrc.header.maps) - 1,
@@ -132,22 +147,19 @@ def resample(mrc, padding=0, out_name='resample.mrc', overwrite=False, new_voxel
     new_data_grid = interpolator(new_grid).reshape(x.shape).astype(np.float32)
     new_data_grid = np.pad(new_data_grid, pad_width=padding)
 
-    try:
-        with mrcfile.new(out_name, overwrite=overwrite) as mrc2:
-            mrc2.header.cella.x = (new_data_grid.shape[axis_mapping[0]]) * new_voxel_size
-            mrc2.header.cella.y = (new_data_grid.shape[axis_mapping[1]]) * new_voxel_size
-            mrc2.header.cella.z = (new_data_grid.shape[axis_mapping[2]]) * new_voxel_size
-            mrc2.header.origin.x = mrc.header.origin.x - padding * new_voxel_size
-            mrc2.header.origin.y = mrc.header.origin.y - padding * new_voxel_size
-            mrc2.header.origin.z = mrc.header.origin.z - padding * new_voxel_size
-            mrc2.set_data(new_data_grid)
-            mrc2.update_header_from_data()
-            mrc2.update_header_stats()
-    except ValueError:
-        pass
+    with mrcfile.new(out_name, overwrite=overwrite) as mrc_out:
+        mrc_out.header.cella.x = (new_data_grid.shape[axis_mapping[0]]) * new_voxel_size
+        mrc_out.header.cella.y = (new_data_grid.shape[axis_mapping[1]]) * new_voxel_size
+        mrc_out.header.cella.z = (new_data_grid.shape[axis_mapping[2]]) * new_voxel_size
+        mrc_out.header.origin.x = mrc.header.origin.x - padding * new_voxel_size
+        mrc_out.header.origin.y = mrc.header.origin.y - padding * new_voxel_size
+        mrc_out.header.origin.z = mrc.header.origin.z - padding * new_voxel_size
+        mrc_out.set_data(new_data_grid)
+        mrc_out.update_header_from_data()
+        mrc_out.update_header_stats()
 
 
-def process_database(datadir_name="../data/pdb_em"):
+def process_database(datadir_name="../data/pdb_em", overwrite=False):
     files_list = os.listdir(datadir_name)
 
     fail_list = []
@@ -159,10 +171,10 @@ def process_database(datadir_name="../data/pdb_em"):
             pdb_path = os.path.join(datadir_name, dirname, f"{pdb_name}.mmtf.gz")
             mrcgz_path = os.path.join(datadir_name, dirname, f"emd_{mrc}.map.gz")
             carved_name = os.path.join(datadir_name, dirname, f"{mrc}_carved.mrc")
-            resampled_name = os.path.join(datadir_name, dirname, f"{mrc}_resampled.mrc")
+            resampled_name = os.path.join(datadir_name, dirname, f"{mrc}_resampled_3.mrc")
 
-            carve(mrc=mrcgz_path, pdb_name=pdb_path, out_name=carved_name, filter_cutoff=6)
-            resample(mrc=carved_name, out_name=resampled_name, padding=0)
+            carve(mrc=mrcgz_path, pdb_name=pdb_path, out_name=carved_name, overwrite=overwrite)
+            resample(mrc=carved_name, out_name=resampled_name, padding=0, new_voxel_size=3, overwrite=overwrite)
         except Exception as e:
             print(e)
             fail_list.append(dirname)
@@ -172,9 +184,9 @@ def process_database(datadir_name="../data/pdb_em"):
 if __name__ == '__main__':
     pass
     datadir_name = "../data/pdb_em"
-    # dirname = '3IXX_5103'  # weird symmetry
+    dirname = '3IXX_5103'  # weird symmetry
     # dirname = '3J70_5020'  # crappy resolution + level goes to 8, what to do with those ?
-    dirname = '3JCX_6629'  # looks ok
+    # dirname = '3JCX_6629'  # looks ok
     # dirname = '3IY2_5107'  # looks ok
     pdb_name, mrc = dirname.split("_")
 
@@ -184,8 +196,8 @@ if __name__ == '__main__':
     # mrc_path = os.path.join(datadir_name, dirname, f"emd_{mrc}.map")
     # carved_name = os.path.join(datadir_name, dirname, f"{mrc}_carved.mrc")
     # resampled_name = os.path.join(datadir_name, dirname, f"{mrc}_resampled_4.mrc")
-    # carve(mrc=mrcgz_path, pdb_name=pdb_path, out_name=carved_name, filter_cutoff=6)
+    # carve(mrc=mrcgz_path, pdb_name=pdb_path, out_name=carved_name, filter_cutoff=6, overwrite=True)
     # resample(mrc=carved_name, out_name=resampled_name, padding=0, new_voxel_size=4, overwrite=True)
 
-    # process_database()
+    process_database(overwrite=True)
     # ['5A8H_3096', '7CZW_30519', '7SJO_25163']
