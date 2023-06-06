0b0d6b8fa4d00d62f1b8a4b2c301f11983a1e75a
Vincentx15 <vincent.mallet96@gmail.com>
2023-06-06 17:12:03 +0200

    push df loading in get_item

--- load_data/ABDataset.py
+++ load_data/ABDataset.py
@@ -22,19 +22,26 @@ class ABDataset(Dataset):
                  rotate=True,
                  crop=0):
         self.data_root = data_root
-        self.df = pd.read_csv(csv_to_read)[["pdb_id", "mrc_id", "dirname", "local_ab_id", "antibody_selection"]]
+        self.csv_to_read = csv_to_read
+        df = pd.read_csv(self.csv_to_read)[
+            ["pdb_id", "mrc_id", "dirname", "local_ab_id", "antibody_selection"]]
+        self.length = len(df)
+        self.df = None
         self.rotate = rotate
         self.return_sdf = return_sdf
         self.return_grid = return_grid
         self.crop = crop
 
     def __len__(self):
-        return len(self.df)
+        return self.length
 
     def unwrapped_get_item(self, item):
         """
         Just useful to desactivate the try/except for debugging
         """
+        if self.df is None:
+            self.df = pd.read_csv(self.csv_to_read)[
+                ["pdb_id", "mrc_id", "dirname", "local_ab_id", "antibody_selection"]]
         row = self.df.loc[item].values
         pdb_id, mrc_id, dirname, local_ab_id, antibody_selection = row
         pdb_path = os.path.join(self.data_root, dirname, f'{pdb_id}.cif')
@@ -54,8 +61,6 @@ class ABDataset(Dataset):
         return dirname, comp
 
     def __getitem__(self, item):
-        row = self.df.loc[item].values
-        pdb_id, mrc_id, dirname, local_ab_id, antibody_selection = row
         # return self.unwrapped_get_item(item)
         try:
             return self.unwrapped_get_item(item)
